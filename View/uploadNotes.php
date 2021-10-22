c2db470f669bf2b89aaecb2e995e095a7eb4a764
Saiydur Rahman <57300418+Saiydur@users.noreply.github.com>
2021-10-22 23:24:17 +0600

    Error Fixed In Views

--- View/uploadNotes.php
+++ View/uploadNotes.php
@@ -17,9 +17,9 @@
     <title>Upload-Notes</title>
 </head>
 <body>
-<?php include('../Global/Header.php');
-include('../Global/UserHeader.php');
-?>
+<?php include('../Global/Header.php');?>
+    <div class="bg-light">
+        <?php include('../Global/UserHeader.php')?>
         <div class="col-md-9 col-lg-10 pl-0 pr-0">
             <div class="jumbotron jumbotron-fluid bg-light mb-0">
                 <div class="container">
@@ -28,7 +28,6 @@ include('../Global/UserHeader.php');
                 </div>
             </div>
         </div>
-    </div>
-</div>
+    </div>    
 </body>
 </html>
