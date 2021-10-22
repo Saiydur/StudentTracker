3fa7855d221c40f3c581cba7676a3add450a2e3f
Saiydur Rahman <57300418+Saiydur@users.noreply.github.com>
2021-10-22 11:29:55 +0600

    Adding New Views

--- View/profile.php
+++ View/profile.php
@@ -13,20 +13,35 @@
 <script src="https://cdn.jsdelivr.net/jquery.validation/1.15.1/jquery.validate.min.js"></script>
 <link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
       <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <title>Blogs</title>
+    <title>Profile</title>
 </head>
 <body>
-    <?php include('../Global/Header.php')?>
-    <!-- Heading Section End Here -->
-    <h1 class="container py-4"><?php
-    if(isset($_SESSION['name'])){
-    $name=explode('@',$_SESSION['name']);
-    echo $name[0]." Thank You For Testing";
-    }
-    else{
-        echo "No Data Found";
-    }
-    ?>
-    </h1>
+<?php include('../Global/Header.php')?>
+    <div class="row">
+        <div class="col-md-3 col-lg-2 ml-auto">
+            <ul class="nav flex-column nav-link">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Profile</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Upload Notes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Upload Marks</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Post Blog</a>
+                </li>
+            </ul>
+        </div>
+        <div class="col-md-9 col-lg-10 ml-auto">
+            <div class="jumbotron jumbotron-fluid bg-success">
+                <div class="container">
+                    <?php include('./todolist.php')?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
 </html>
---
+++ View/todolist.php
@@ -0,0 +1,3 @@
+<section>
+    <h1>This is Todo-List Section</h1>
+</section>
