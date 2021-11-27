28f68d96f62d483c9d1e3c891c03f3a76c0e523d
Sayidur <saiydurrahman28081999@gmail.com>
2021-11-27 14:19:37 +0600

    Adding JS Validation at Registration Module

--- View/LoginForm.php
+++ View/LoginForm.php
@@ -86,7 +86,7 @@
                     </div>
                 </div>
                 <!-- bootstrapcdn script -->
-                <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+                <script src="https://code.jquery.c~om/jquery-3.2.1.slim.min.js"
                     integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
                     crossorigin="anonymous">
                 </script>
