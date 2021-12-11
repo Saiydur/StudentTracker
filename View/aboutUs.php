81b37254b4f4578ea4843d2da7a37609d9777ab4
safkatjaman <19-40286-1@student.aiub.edu>
2021-12-12 01:04:57 +0600

    Added css on about us and changed the cover photo

--- View/aboutUs.php
+++ View/aboutUs.php
@@ -7,16 +7,70 @@
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <script src="https://cdn.jsdelivr.net/jquery.validation/1.15.1/jquery.validate.min.js"></script>
 <link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
       <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <title>About Us</title>
+      <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+      <title>About Us</title>
 </head>
 <body>
     <?php include('../Global/Header.php')?>
     <div class="container px-3">
-        <h1>This Is About Us Page</h1>
+        <div style="text-align: center;" >
+            <img style="width: 35%; height:auto" src="../src//Images//Alpha Alliance (2).png" alt="">
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-6 container-fluid vh-100" data-aos="fade-right" data-aos-duration="2000">
+                <img style="width: 100%;" src="../src/Images/p11.png" class="rounded float-start" alt="saiydur rahman">
+            </div>
+
+            <div class="col-6 container-fluid vh-100" data-aos="fade-left" data-aos-duration="2000">
+                <h2>Saiydur Rahman</h2>
+                <h4>American International University-Bangladesh</h4>
+                <h4>B.Sc. in CSE</h4>
+                <h4>Software Engineer</h4>
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="col-6" data-aos="zoom-in-right" data-aos-duration="2000">
+                <h2>Safkat Jaman</h2>
+                <h4>American International University-Bangladesh</h4>
+                <h4>B.Sc. in CSE</h4>
+                <h4>Data Analyst</h4>
+            </div>
+
+            <div class="col-6" data-aos="zoom-in-left" data-aos-duration="2000">
+                <img style="width: 100%;" src="../src/Images/p12.png" class="rounded float-end" alt="safkat jaman">
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="col-6" data-aos="zoom-out-right" data-aos-duration="2000">
+                <img style="width: 100%;" src="../src/Images/p13.png" class="rounded float-start" alt="al jaber">
+            </div>
+
+            <div class="col-6" data-aos="zoom-out-left" data-aos-duration="2000">
+                <h2>Md. Abdullah Al-Jaber</h2>
+                <h4>American International University-Bangladesh</h4>
+                <h4>B.Sc. in CSE</h4>
+                <h4>Software Engineer</h4>  
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="col-6" data-aos="fade-left" data-aos-duration="2000">
+                <h2>Rafidur Rahman</h2>
+                <h4>American International University-Bangladesh</h4>
+                <h4>B.Sc. in CSE</h4>
+                <h4>Software Engineer</h4>
+            </div>
+
+            <div class="col-6" data-aos="fade-right" data-aos-duration="2000">
+                <img style="width: 100%;" src="../src//Images/p14.png" class="rounded float-end" alt="rafidur rahman">
+            </div>
+        </div>
     </div>
     <!-- bootstrapcdn script -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
@@ -27,6 +81,9 @@
     </script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
+    <script>
+        AOS.init();
     </script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -66,13 +66,13 @@
                 </ol>
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img class="d-block w-100" src="./src/Images/img1.png" alt="First slide">
+                        <img class="d-block w-100" src="./src/Images/1.png" alt="First slide">
                     </div>
                     <div class="carousel-item">
-                        <img class="d-block w-100" src="./src/Images/img1.png" alt="Second slide">
+                        <img class="d-block w-100" src="./src/Images/2.png" alt="Second slide">
                     </div>
                     <div class="carousel-item">
-                        <img class="d-block w-100" src="./src/Images/img1.png" alt="Third slide">
+                        <img class="d-block w-100" src="./src/Images/3.png" alt="Third slide">
                     </div>
                 </div>
                 <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
